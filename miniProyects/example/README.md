3c6e7ffeb20f0587f071d1da6c0a9799355fb0ae
unknown <juanluislopez24@gmail.com>
2019-01-21 17:56:57 -0600

    added base example

---
+++ miniProyects/example/README.md
@@ -0,0 +1,12 @@
+# Example
+Here is an example of what I expect.
+
+I followed this example to set my environment:
+
+https://code.visualstudio.com/docs/java/java-tutorial 
+
+This has
+
+
+
+
---
+++ miniProyects/example/src/app/Tests.java
@@ -0,0 +1,20 @@
+package app;
+
+/**
+ * Tests
+ */
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class Tests {
+  @Test
+  public void thisShouldSucceed() {
+    assertTrue(true);
+  }
+
+  @Test
+  public void thisShouldFail() {
+    assertTrue(false);
+  }
+}
